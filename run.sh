3f032d673be5ac9b06d8d7ff49b7b8fa919badcf
rohit01 <hello@rohit.io>
2016-05-02 16:26:33 +0530

    run.sh fix

--- run.sh
+++ run.sh
@@ -5,17 +5,21 @@ set -e
 export BOSUN_CONF_tsdbHost=${BOSUN_CONF_tsdbHost:-localhost}
 export BOSUN_CONF_stateFile=${BOSUN_CONF_stateFile:-/tmp/bosun.state}
 
-if [ ! -e /bosun/bosun.conf ]; then
-    touch /bosun/bosun.conf
+CONFIG_FILE="/bosun/bosun.conf"
+rm -f ${CONFIG_FILE}
+
+if [ ! -e ${CONFIG_FILE} ]; then
+    touch ${CONFIG_FILE}
 
     for VAR in $(env); do
-        if [[ $VAR =~ ^BOSUN_CONF_ ]]; then
-          BOSUN_conf_name=$(echo "$VAR" | sed -r 's/^BOSUN_CONF_([^=]*)=.*/\1/' | sed 's/__/./g'')
-          BOSUN_conf_value=$(echo "$VAR" | sed -r "s/^[^=]*=(.*)/\1/")
+    	echo "$VAR"
+        if echo "$VAR" | grep "^BOSUN_CONF_" >/dev/null; then
+          BOSUN_conf_name="$(echo "$VAR" | sed -r 's/^BOSUN_CONF_([^=]*)=.*/\1/' | sed 's/__/./g')"
+          BOSUN_conf_value="$(echo "$VAR" | sed -r "s/^[^=]*=(.*)/\1/")"
 
-          echo "$BOSUN_conf_name = $BOSUN_conf_value" >> /etc/opentsdb/opentsdb.conf
+          echo "$BOSUN_conf_name = $BOSUN_conf_value" >> ${CONFIG_FILE}
         fi
     done
 fi
 
-exec /go/bin/bosun -c /bosun/bosun.conf
+exec /go/bin/bosun -c ${CONFIG_FILE}
